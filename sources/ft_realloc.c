4a7c87db1d13f9487088d3c6f487e58ef3045957
stanlyan <one2jeen@gmail.com>
2020-07-27 13:51:20 +0300

    fix(ft_realloc): free ptr on success

--- sources/ft_realloc.c
+++ sources/ft_realloc.c
@@ -1,7 +1,8 @@
 /*
 ** DESCRIPTION
 ** The ft_realloc() function changes the size of the memory block bointed to by
-** ptr to new_size bytes.
+** ptr to new_size bytes. If allocation takes place or new_size is zero then
+** ptr is freed.
 **
 ** RETURN VALUE
 ** The ft_memalloc() function returns a pointer to the memry area of new_size
@@ -27,7 +28,10 @@ void	*ft_realloc(void *ptr, size_t old_size, size_t new_size)
 		free(ptr);
 		return (NULL);
 	}
-	if (!(x = malloc(sizeof(new_size))))
-		return (NULL);
-	return (ft_memmove(x, ptr, new_size < old_size ? new_size : old_size));
+	if ((x = malloc(new_size))
+	{
+		x = ft_memmove(x, ptr, new_size < old_size ? new_size : old_size);
+		free(ptr);
+	}
+	return (x);
 }
